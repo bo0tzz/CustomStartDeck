e81bc9564d1a2fd76f1cb07f50141c60478999c3
bo0tzz <btdewilligen@gmail.com>
2021-10-13 18:28:06 +0200

    Add boilerplate

--- Plugin.cs
+++ Plugin.cs
@@ -1,13 +1,16 @@
 ﻿using BepInEx;
+using HarmonyLib;
 
 namespace CustomStartRelics
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Peglin.exe")]
     public class Plugin : BaseUnityPlugin
     {
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
         private void Awake()
         {
-            // Plugin startup logic
+            harmony.PatchAll();
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
